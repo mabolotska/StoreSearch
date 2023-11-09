08033cd415bcd6adfd203e33ebbe63822fa2e690
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2023-11-09 18:58:25 +0100

    A new “No results” cell

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Maryna Bolotska on 09/11/23.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
